66835fecfe2f697be54f79a8630b7bd6ea027516
tuda224 <johann.tuder@gmail.com>
2021-02-15 20:51:35 +0100

    initial commit

---
+++ CC_HyperloopTraining/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CC_HyperloopTraining
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                ReadInput();
+            }
+        }
+
+        private static void ReadInput()
+        {
+            Console.WriteLine("Using input file: C:\\temp\\input.txt\nPress Return for run.");
+            Console.ReadLine();
+            var lines = File.ReadLines("C:\\temp\\input.txt").ToArray();
+
+            var lineCounter = 0;
+        }
+
+        private static int GetInt(string s)
+        {
+            return int.Parse(s);
+        }
+
+        private static void PrintResult(string s)
+        {
+            PrintResult(new List<string> {
+                s
+            });
+        }
+
+        private static void PrintResult(IEnumerable<string> lines)
+        {
+            File.WriteAllLines("C:\\temp\\result.txt", lines);
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
